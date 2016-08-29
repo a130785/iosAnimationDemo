cc1ce39614a7c95a6ea84f3b8b70e1d60250d3f4
邬维 <401260566@qq.com>
2016-08-29 08:55:34 +0800

    sss

--- 自定义登录界面/NextViewController.m
+++ 自定义登录界面/NextViewController.m
@@ -11,6 +11,9 @@
 
 @interface NextViewController ()<UIViewControllerTransitioningDelegate>
 
+@property(nonatomic ,strong)CAShapeLayer *loadingLayer;
+@property(nonatomic ,strong)NSTimer *timer;
+
 @end
 
 @implementation NextViewController
@@ -33,6 +36,9 @@
     imageView.userInteractionEnabled = YES;
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(pop:)];
     [imageView addGestureRecognizer:tap];
+    //有兴趣的可以把上面注释，把这里放开，看看效果
+//    [self drawCircle];
+//    [self drawHalfCircle];
 
 }
 
@@ -40,6 +46,7 @@
     [super didReceiveMemoryWarning];
 }
 
+#pragma -mark UIViewControllerTransitioningDelegate
 -(id<UIViewControllerAnimatedTransitioning>)animationControllerForPresentedController:(UIViewController *)presented presentingController:(UIViewController *)presenting sourceController:(UIViewController *)source{
     return [FourPingTransition transitionWithTransitionType:XWPresentOneTransitionTypePresent];
 }
@@ -51,6 +58,56 @@
 -(void)pop:(UITapGestureRecognizer *)sender
 {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+
+#pragma -mark 画loading
+- (CAShapeLayer *)drawCircle {
+    CAShapeLayer *circleLayer = [CAShapeLayer layer];
+    // 指定frame，只是为了设置宽度和高度
+    circleLayer.frame = CGRectMake(0, 0, 200, 200);
+    // 设置居中显示
+    circleLayer.position = self.view.center;
+    // 设置填充颜色
+    circleLayer.fillColor = [UIColor clearColor].CGColor;
+    // 设置线宽
+    circleLayer.lineWidth = 2.0;
+    // 设置线的颜色
+    circleLayer.strokeColor = [UIColor redColor].CGColor;
+    
+    // 使用UIBezierPath创建路径
+    CGRect frame = CGRectMake(0, 0, 200, 200);
+    UIBezierPath *circlePath = [UIBezierPath bezierPathWithOvalInRect:frame];
+    
+    // 设置CAShapeLayer与UIBezierPath关联
+    circleLayer.path = circlePath.CGPath;
+    return circleLayer;
+}
+
+- (void)drawHalfCircle {
+    self.loadingLayer = [self drawCircle];
+    
+    // 这个是用于指定画笔的开始与结束点
+    self.loadingLayer.strokeStart = 0.0;
+    self.loadingLayer.strokeEnd = 0.1;
+    // 将CAShaperLayer放到某个层上显示
+    [self.view.layer addSublayer:_loadingLayer];
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1
+                                                  target:self
+                                                selector:@selector(updateCircle)
+                                                userInfo:nil
+                                                 repeats:YES];
+}
+
+- (void)updateCircle {
+    self.loadingLayer.strokeEnd += 0.1;
+    
+    if (self.loadingLayer.strokeEnd >= 1) {
+        self.loadingLayer.strokeEnd = 0;
+        [self.timer invalidate];
+        self.timer = nil;
+    }
 }
 
 @end
